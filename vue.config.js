a47ed92992777505fa4f168da201e0987e8bc9d4
Cameron Bell <Cameron.Bell@k12.wa.us>
2020-10-29 11:23:30 -0700

    ReleaseAttempt5

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   chainWebpack: config => {
+    devtool: 'source-map',
     config
       .plugin('case-sensitive-paths')
       .use(require('case-sensitive-paths-webpack-plugin'));
